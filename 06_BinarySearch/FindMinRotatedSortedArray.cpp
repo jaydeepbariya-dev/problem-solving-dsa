4e64de6d250e8a24b9bffd0ff88c9ed192c92cf4
jaydeepbariya-dev <jaydeepbariya.dev@gmail.com>
2025-05-06 09:42:39 +0530

    Create FindMinRotatedSortedArray.cpp

---
+++ 06_BinarySearch/FindMinRotatedSortedArray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int start = 0, end = nums.size() - 1;
+        int ans = nums[0]; // What if array not rotated
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] >= nums[0]) {
+                start = mid + 1;
+            } else {
+                ans = nums[mid];
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
